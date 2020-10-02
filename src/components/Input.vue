b7136e0889ac743e592fbe46a7297e8718228cfc
gormrgryn <gor-margaryan-05@mail.ru>
2020-10-02 16:30:56 +0400

    added forms, question add/remove buttons

---
+++ src/components/Input.vue
@@ -0,0 +1,81 @@
+<template>
+    <div id="input">
+        <div class="tdi_name_cont tdi_cont">
+            <div>
+                <input type="text" class="tdinput" id="tdinput_name" required autocomplete="off"/>
+                <label for="tdninput" class="form__label" id="name__label">ToDo name</label>
+            </div>
+        </div>
+        <div class="tdi_name_cont tdi_cont">
+            <div>
+                <input type="text" class="tdinput" id="tdinput_name" required autocomplete="off"/>
+                <label for="tdninput" class="form__label" id="name__label">ToDo name</label>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'input-quest'
+}
+</script>
+
+<style>
+#input {
+    margin-left: 2em;
+    margin-top: 4em;
+    display: inline-block;
+    width: 38em;
+}
+.tdi_cont + .tdi_cont {
+    margin-left: 2em
+}
+.tdinput {
+	border: 0;
+	outline: 0;
+	color: #fff;
+	background: transparent;
+    transition: border-color 0.2s;
+    display: block;
+	transition: 0.2s;
+	font-size: 1em;
+    color: #086375;
+    height: 2em;
+    font-family: inherit;
+    border-bottom: 1px solid #086375;
+}
+#tdinput_name:focus {
+    margin-top: .5em;
+}
+.tdinput:focus {
+	border-width: 2px;
+	border-image-slice: 1;
+}
+.tdinput:placeholder-shown ~ .form__label {
+	font-size: 1.3rem;
+	cursor: text;
+    position: relative;
+    bottom: 1.5em;
+    transition: 0.2s
+}
+.tdinput:focus ~ .form__label {
+    position: relative;
+    bottom: 3.3em;
+    display: block;
+	transition: 0.2s;
+	font-size: 1rem;
+	color: #11998e;
+	font-weight: 700;
+}
+.tdi_cont {
+    width: 17em;
+    display: inline-flex;
+    justify-content: center;
+    background-color: #1dd3b0;
+    border-radius: 50em;
+}
+.form__label {
+    opacity: 0;
+}
+</style>
--- src/views/QForm.vue
+++ src/views/QForm.vue
@@ -1,17 +1,82 @@
 <template>
-  <sub-form></sub-form>
+  <div>
+    <div ref="scount">
+      <div v-for="f in forms" :key="f">
+        <input-quest></input-quest>
+        <span class="delinp" @click="remForm" v-show="remShow">x</span>
+      </div>
+    </div>
+
+    <div class="addsf" @click="addForm">
+      <span>+</span>
+    </div>
+  </div>
 </template>
 
 <script>
-import SubForm from '../components/SubForm.vue'
+import InputQuest from "../components/Input"
 export default {
-  name: 'QForm',
+  name: "QForm",
   components: {
-    'sub-form': SubForm
+    "input-quest": InputQuest
+  },
+  data() {
+    return {
+      forms: 1, // using this property just to add divs with v-for loop
+      remShow: false
+    };
+  },
+  mounted() {
+    this.updRem()
+  },
+  methods: {
+    updRem() { // not able to use this.$refs in computed, this function checks remShow value
+      setTimeout(() => {
+        this.remShow = this.$refs.scount.children.length == 1 ? false : true
+      })
+      console.log(this.remShow)
+      console.log(this.$refs.scount.children)
+      console.log(this.$refs.scount)
+    },
+    addForm() {
+      ++this.forms
+      this.updRem()
+    },
+    remForm(e) {
+      e.target.parentNode.remove() // do not decrement this.forms, strange thing hapenning
+      this.updRem()
+    }
   }
-}
+};
 </script>
 
 <style>
-
+.addsf {
+  font-size: 3em;
+  background-color: #3c1642;
+  color: #affc41;
+  position: fixed;
+  bottom: 0.7em;
+  right: 1em;
+  border-radius: 5em;
+  width: 1.5em;
+  height: 1.5em;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  box-shadow: 0 0 6px #3c1642;
+  transition: 0.3s;
+}
+.addsf:hover {
+  box-shadow: 0 0 12px #3c1642;
+}
+.delinp {
+  color: #3c1642;
+  display: inline-block;
+  font-size: 2em;
+  position: relative;
+  top: 0.35em;
+  cursor: pointer;
+}
 </style>
